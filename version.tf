4d167a9eebd456158b20e618329b0f4838be0ec9
Deivid Duarte <deividdua@gmail.com>
2024-05-16 18:55:57 -0300

    feat: add provider and version terraform

---
+++ version.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.49.0"
+    }
+    # helm = {
+    #   source = "hashicorp/helm"
+    #   version = "2.13.2"
+    # }
+  }
+}
