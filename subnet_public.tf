1e61ecd5a9229abc1f786902fa804e52552ca463
Deivid Duarte <deividdua@gmail.com>
2024-05-16 20:18:10 -0300

    feat: subnets publics

---
+++ subnet_public.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "projeto_eks_subnet_1a"
+  }
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "projeto_eks_subnet_1b"
+  }
+}
