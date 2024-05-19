6850b245c3fc437d333a3c9c44dc93c5c73b9938
Deivid Duarte <deividdua@gmail.com>
2024-05-18 23:21:38 -0300

    feat: add providers kubernetes and helm

--- modules/aws-load-balancer-controller/variables.tf
+++ modules/aws-load-balancer-controller/variables.tf
@@ -7,3 +7,13 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to AWS resources"
 }
+
+variable "oidc" {
+  type        = string
+  description = "HTTPS URL from OIDC Provider of the EKS cluster"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+}
